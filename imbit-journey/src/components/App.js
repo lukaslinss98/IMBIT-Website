821821406e73b0394fcc8b901f3e8ee19831ff43
lukas <lukaslinss98@gmail.com>
2022-10-17 09:42:42 +0200

    changed folder structure

--- imbit-journey/src/components/App.js
+++ imbit-journey/src/components/App.js
@@ -1,14 +1,11 @@
-import logo from './images/DHBW-Logo.svg.png';
-import './App.css';
+import logo from './../images/DHBW-Logo.svg.png';
+import './../styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
       </header>
     </div>
   );
